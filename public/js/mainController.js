01ba734d480aced3ec13eeb096ba6870f9063e76
thornb <bthorne3@gmail.com>
2017-04-23 14:00:07 -0400

    sorted rounds and display rank

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -183,7 +183,19 @@ angular.module("myApp", []).controller("mainController", ['$scope','$http', func
 
 		$scope.$apply(function(){
 			$scope.killerLeaderboard = msg['killerLeaderboard'];
+
+			//sort
+			$scope.killerLeaderboard.sort(function(a, b){
+				return a['round'] - b['round'];
+			});
+
 			$scope.prisonerLeaderboard = msg['prisonerLeaderboard'];
+
+			//sort
+			$scope.prisonerLeaderboard.sort(function(a, b){
+				return b['round'] - a['round'];
+			});
+
 		});
 
 	});
